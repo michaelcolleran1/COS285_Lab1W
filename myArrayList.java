8d54a5de0247f8cba7b948f06e0cb668f41d7aab
michaelcolleran1 <144271462+michaelcolleran1@users.noreply.github.com>
2023-09-20 14:16:41 -0400

    Add files via upload

---
+++ myArrayList.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;2
+import java.util.List;
+
+public class myArrayList extends Fraction {
+	
+	
+	public myArrayList(int num, int denom) {
+		super(num, denom);
+		// TODO Auto-generated constructor stub
+	}
+
+	ArrayList<Fraction> arrayList = new ArrayList<>(50);
+		
+	public int setGrowth(List array, int percentage) {
+		int currentsize = array.size();
+		
+				
+	}
+	
+}
+s
