d097d38607500f435d675e67787dd2155df32594
maxrogers <maxrogers@maxs-mbp.lan>
2023-09-19 21:39:25 -0400

    Updated comment. Indicates that the method has been moved to main

---
+++ booleanAdd.java
@@ -0,0 +1,14 @@
+//METHOD IS NOW LOCATED IN MAIN
+
+
+//public boolean booleanAddMethod(ArrayList<Fraction> aL, Fraction f){
+//		int newNum = (this.getNum() * f.getDenom()) + (f.getNum() * this.getDenom());
+//        int newDenom = this.getDenom() * f.getDenom();
+//		if(aL.add(new Fraction(newNum, newDenom)))
+//			return true;
+//		return false;
+
+
+	
+	
+
